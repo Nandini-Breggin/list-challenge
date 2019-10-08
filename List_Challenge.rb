947e7be4f9ef13b6a70466705b2b05cab82fe5a4
Nandini Breggin <nandinibreggin@gmail.com>
2019-10-08 09:29:50 -0600

    Last commit before the quiz

--- List_Challenge.rb
+++ List_Challenge.rb
@@ -129,62 +129,62 @@ end
 # puts either_2_4([1, 2, 2, 4, 5]) # true
 # puts either_2_4([0, 3, 4]) # false
 
-
-
-def merge(a, b)
-
+def merge(a,b)
+    
+    a.reverse!
+    b.reverse!
     result = []
-    if a[0] < b[0]
-        result << a.shift
-    else
-        result << b.shift
+  
+      # loop till either a or b is empty
+    loop do
+      break if a.empty? || b.empty?
+      # compare each value in the list to the other list value
+      # pop = removes the last element or returns nil
+      # the value is pushed to the result
+      result << (a.last < b.last ? a.pop : b.pop)
     end
+    return result + a.reverse + b.reverse
+end
+  
+# print merge([1, 3, 7, 11, 12, 35], [2, 4, 6, 14]) #[1, 2, 3, 4, 6, 7, 11, 12, 14, 35]
 
-    if a.length == 0
-        return result + b
-    elsif b.length == 0
-        return result + a
-    end
+def shift_left(list)
+    # rotate moves the value
+   print list.rotate
 end
 
-merge([1, 2, 5], [4, 8, 10]) # [1, 2, 4, 8, 9, 10]
+# shift_left([2, 4, 5]) # 4, 5, 2
 
 
 
+def max_span(list)
+    ans = 0
+    # Counts the amount in between the first and last item in the list.
+    if list.count >= 2
+        ans = list.count - 2
+    else
+        ans = list.count
+    end
+    print ans
+end
+
+# puts max_span([4])
 
 
-# def max_span
-# end
+def count_code(str)
+
+    count = 0
+    (str.size - 3).times do |i|
+        slice = str[i.. i + 3] 
+        if slice == "coae" || slice == "cobe" || slice == "coce" || slice == "code" || slice == "coee" || slice == "cofe" || slice == "coge" || slice == "cohe" || slice == "coie" || slice == "coje" || slice == "coke" || slice == "cole" || slice == "come" || slice == "cone" || slice == "cooe" || slice == "cope" || slice == "coqe" || slice == "core" || slice == "cose" || slice == "cote" || slice == "coue" || slice == "cove" || slice == "cowe" || slice == "coxe" || slice == "coye" || slice == "coze"
+            count += 1
+        end
+    end
+    return count
+end
+
+# puts count_code("codecopecade") # 2
+# puts count_code("catdogcode") # 1
 
 
-
-
-# def count_code
-
-# end
-
-
-
-
-# ? def get_sandwhich(str)
-# f
-# end
-
-# puts get_sandwhich("breadjambread") # jam
-# puts get_sandwhich("ketchup") # ___
-# puts get_sandwhich("breadjambreadbutterbreadaa") # jambreadbutter
-
-
-
-
-
-# def shift_left(list)
-#    i = 0
-#    shifted = []
-#    two = []
-#    two[0] = shifted[0]
-#    list.each do |n|
-
-# end
-
-# print shift_left([6, 2, 5, 3]) # [2, 5, 3, 6]
+# get_sandwhich
--- list-chall-amma.rb
+++ list-chall-amma.rb
@@ -1,22 +1,71 @@
-def merge(a, b)
-    # result = Array.new
-    print a[0]
+
+
+
+def shift_left(list)
+    # rotate moves the value
+   print list.rotate
 end
 
-merge([1, 2, 5], [4, 8, 10]) # [1, 2, 4, 8, 9, 10]
+# shift_left([2, 4, 5]) # 4, 5, 2
 
 
+def count_code(str)
+
+    count = 0
+    (str.size - 3).times do |i|
+        slice = str[i.. i + 3] 
+        if slice == "coae" || slice == "cobe" || slice == "coce" || slice == "code" || slice == "coee" || slice == "cofe" || slice == "coge" || slice == "cohe" || slice == "coie" || slice == "coje" || slice == "coke" || slice == "cole" || slice == "come" || slice == "cone" || slice == "cooe" || slice == "cope" || slice == "coqe" || slice == "core" || slice == "cose" || slice == "cote" || slice == "coue" || slice == "cove" || slice == "cowe" || slice == "coxe" || slice == "coye" || slice == "coze"
+            count += 1
+        end
+    end
+    return count
+end
+
+# puts count_code("codecopecade") # 2
+# puts count_code("catdogcode") # 1
 
 
-# result = []
-#     if a[0] < b[0]
-#         result << a.shift
-#     else
-#         result << b.shift
-#     end
+def merge(a,b)
+    
+    a.reverse!
+    b.reverse!
+    result = []
+  
+      # loop till either a or b is empty
+    loop do
+      break if a.empty? || b.empty?
+      # compare each value in the list to the other list value
+      # pop = removes the last element or returns nil
+      # the value is pushed to the result
+      result << (a.last < b.last ? a.pop : b.pop)
+    end
+    return result + a.reverse + b.reverse
+  end
+  
+  # print merge([1, 3, 7, 11, 12, 35], [2, 4, 6, 14])
 
-#     if a.length == 0
-#         return result + b
-#     elsif b.length == 0
-#         return result + a
-#     end
+def max_span(list)
+    ans = 0
+    # Counts the amount in between the first and last item in the list.
+    if list.count >= 2
+        ans = list.count - 2
+    else
+        ans = list.count
+    end
+    print ans
+end
+
+puts max_span([4])
+
+
+def get_sandwich(str)
+
+    count = 0
+    (str.size - 2).times do |i|
+        slice = str[i.. i + 2] 
+        if slice == "cat"
+            count += 1
+        end
+    end
+    return count
+end
